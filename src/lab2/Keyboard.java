6c79264bb29487487c6f263abff1399af792b7d6
Moody <asdas201888@gmail.com>
2022-10-20 20:53:40 +0200

    update in Keyboard.java still have some todos ***** missed cursor move in textArea and correct backspace

--- src/lab2/Keyboard.java
+++ src/lab2/Keyboard.java
@@ -11,11 +11,11 @@ import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 public class Keyboard extends JFrame implements KeyListener, ActionListener {
+    private static final int KEYS_SIZE = 56;
     ///text area
     private static JTextArea textArea;
-    private static JScrollPane textPane;
-    private static final JButton[] buttons = new JButton[57];
-    private static final JPanel keyHolder = new JPanel(new BorderLayout(5, 5));
+    private static final JButton[] buttons = new JButton[KEYS_SIZE];
+    private static final JPanel keyHolder = new JPanel(new BorderLayout(8,8));
     private static final JPanel innerKeyHolder1 = new JPanel();
     private static final JPanel innerKeyHolder2 = new JPanel();
     private static boolean capsFlag;
@@ -26,10 +26,9 @@ public class Keyboard extends JFrame implements KeyListener, ActionListener {
 
         ///text area
         textArea = new JTextArea("");
-        textPane = new JScrollPane(textArea);
+        JScrollPane textPane = new JScrollPane(textArea);
         ///margin
-        textPane.setBorder(new EmptyBorder(10, 10, 1, 10));
-//        keyHolder.setBorder(new EmptyBorder(1,1,5,1));
+        textPane.setBorder(new EmptyBorder(16, 16, 1, 16));
         ////////////////B/////////////////////
         setLayout(new GridLayout(2, 1, 40, 10));
 
@@ -38,7 +37,7 @@ public class Keyboard extends JFrame implements KeyListener, ActionListener {
         add(keyHolder);
 
         ////////////////E//////////////////////
-        setSize(700, 400);
+        setSize(720, 400);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
         setResizable(false);
         setLocationRelativeTo(null);  //center
@@ -47,25 +46,25 @@ public class Keyboard extends JFrame implements KeyListener, ActionListener {
 
     private void handleButtons() {
 
-//        System.out.println(helper.length()); // 58
 
-        String helper = "~1234567890-+AAqwertyuiop[]|Aasdfghjkl;'AAzxcvbnm,./AAAAA";
-//              13 14 28 40 41 53 54 55 56
-        for (int i = 0; i < 57; i++) {
+        String helper = "~1234567890-+AAqwertyuiop[]Aasdfghjkl;'AAzxcvbnm,./AAAAA";
+//        System.out.println(helper.length()); // 56
+//              13 14 27 39 40 51 52 53 54 55
+        for (int i = 0; i < KEYS_SIZE; i++) {
 //            if (helper.charAt(i) == 'A') System.out.print(i+" ");
             buttons[i] = new JButton();
 
             if (helper.charAt(i) == 'A') {
                 if (i == 13) buttons[i].setText("Backspace");
                 if (i == 14) buttons[i].setText("CapsLock");
-                if (i == 28) buttons[i].setText("Enter");
-                if (i == 40) buttons[i].setText("Shift");
-                if (i == 41) buttons[i].setText("/");
-                if (i == 52) buttons[i].setText("                                     ");
-                if (i == 53) buttons[i].setText("Left");
-                if (i == 54) buttons[i].setText("Up");
-                if (i == 55) buttons[i].setText("Right");
-                if (i == 56) buttons[i].setText("Down");
+                if (i == 27) buttons[i].setText("Enter");
+                if (i == 39) buttons[i].setText("Shift");
+                if (i == 40) buttons[i].setText("/");
+                if (i == 51) buttons[i].setText("                                           ");
+                if (i == 52) buttons[i].setText("Left");
+                if (i == 53) buttons[i].setText("Up");
+                if (i == 54) buttons[i].setText("Right");
+                if (i == 55) buttons[i].setText("Down");
             } else {
                 buttons[i].setText(helper.charAt(i) + "");
             }
@@ -73,7 +72,7 @@ public class Keyboard extends JFrame implements KeyListener, ActionListener {
             buttons[i].addActionListener(this); ///action listen
             buttons[i].setFocusable(false);
 
-            if (i >= 52)
+            if (i >= 51)
                 innerKeyHolder1.add(buttons[i]);
             else
                 innerKeyHolder2.add(buttons[i]);
@@ -112,10 +111,10 @@ public class Keyboard extends JFrame implements KeyListener, ActionListener {
         if (!(e.getSource() instanceof JButton btn)) return;
 
 //        System.out.println(((JButton)e.getSource()).getText());
-        String text = btn.getText();
+        String textBtn = btn.getText();
 
-        switch (text) {
-            case "Backspace" -> { ///from https://stackoverflow.com/questions/25319104/jtextarea-backspace-and-clear
+        switch (textBtn) {
+            case "Backspace" -> { /// from https://stackoverflow.com/questions/25319104/jtextarea-backspace-and-clear
                 try {
                     Document doc = textArea.getDocument();
                     if (doc.getLength() > 0) {
@@ -134,32 +133,28 @@ public class Keyboard extends JFrame implements KeyListener, ActionListener {
                 else
                     btn.setBackground(Color.lightGray);
             }
-            case "Enter" -> {
-                textArea.append("\n");
-            }
+            case "Enter" -> textArea.append("\n");
             case "Shift" -> {
-
+                // TODO: Shift action
             }
-            case "                                     " -> {
-                textArea.append(" ");
-            }
+            case "                                           " -> textArea.append(" ");
             case "Left" -> {
-
+                // TODO: left action
             }
             case "Up" -> {
-
+                // TODO: up action
             }
             case "Right" -> {
-
+                // TODO: right action
             }
             case "Down" -> {
-
+                // TODO: down action
             }
             default -> {
                 if (capsFlag)
-                    textArea.append(text.toUpperCase());
+                    textArea.append(textBtn.toUpperCase());
                 else
-                    textArea.append(text);
+                    textArea.append(textBtn);
             }
         }
     }
